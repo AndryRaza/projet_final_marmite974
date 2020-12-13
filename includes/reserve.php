84d0d8813d3df5b67193ecd22de5b35ddce93a88
David ADOLPHE <dadoo97441@gmail.com>
2020-12-13 15:26:04 +0400

    ajout fonctionnalité expiré si la date est dépassée il n'est plus possible de reserver

--- includes/reserve.php
+++ includes/reserve.php
@@ -1,6 +1,12 @@
 <?php
 $path = '../data/atelier.json';
 $atelier = json_decode(file_get_contents($path), true);
+
+/**
+ * si le bouton reserver est cliqué
+ * on compare l'id de la carte à ceux du fichier json
+ * quand le bon id est trouvé le nombre de place actuel est décrémenté de 1 
+ */
 if (isset($_POST['reserver'])) {
     $id = $_POST['id'];
     foreach ($atelier as $key => $value) {
