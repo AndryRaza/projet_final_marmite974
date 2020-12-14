cbcc2a2914a0867154db7b14c64782ae3bce11f4
AndryRaza <70998539+AndryRaza@users.noreply.github.com>
2020-12-14 10:33:57 +0400

    Modification connexion

--- includes/connexion.php
+++ includes/connexion.php
@@ -1,21 +1,25 @@
 <?php
 
 session_start();
+$data_file = "../data/membre.json";
+$json_membre = file_get_contents("../data/membre.json");
+$membre = json_decode($json_membre, true);
 
 if (isset($_POST['connexion']))     //Pour gérer la connexion en mode admin
 {
     if ($_POST['mail_user'] === "admin" && $_POST['mdp_user'] === "admin") {
         $_SESSION['admin'] = true;     //On est connecté en mode admin, les vues seront modifiées
-        header('location: ../index.php');   //On redirige vers la page d'accueil
+          
     }
     else 
     {
         foreach ($membre as $key => $value) {
-            if ($value['mail_user'] === $_POST['mail_user'] && $value['mdp_user'] === $_POST['mdp_user']) {
+            if ($value['mail'] === $_POST['mail_user'] && $value['password'] === $_POST['mdp_user']) {
                 $_SESSION['user'] = $value['name'];
             }
         }
     }
+    header('location: ../index.php'); //On redirige vers la page d'accueil
 }
 
 if (isset($_POST['deconnexion'])) {  //Lorsque l'on se déconnecte
@@ -24,8 +28,6 @@ if (isset($_POST['deconnexion'])) {  //Lorsque l'on se déconnecte
     header('location: ../index.php');    //On redirige vers la page d'accueil
 }
 
-$data_file = "../data/membre.json";
-$json_membre = file_get_contents("../data/membre.json");
-$membre = json_decode($json_membre, true);
+
 
 
